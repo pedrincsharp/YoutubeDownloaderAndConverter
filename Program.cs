0af09a886beaf77ec90e0f59f5be75b0d11eda89
Pedrin <joao_pedro_alves_moraes@outlook.com.br>
2023-06-23 14:31:58 -0300

    Subindo projeto completo

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using YoutubeDownloaderAndConverter.YDC;
+
+namespace YoutubeDownloaderAndConverter
+{
+    class Program
+    {
+        static DownloadVideo download;
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Digite a URL do video:");
+            string url = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(url))
+                Console.WriteLine("Url vazia informada!");
+
+            Console.WriteLine("Informe o formato (MP3 - MP4)\nPadrão é MP4 (Deixar vazio)");
+            string formato = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(formato))
+                formato = "MP4";
+
+            formato = formato.ToUpper();
+
+            download = new DownloadVideo(url);
+            await download.GetVideo("C:\\Videos", formato);
+
+            Console.WriteLine("Presione qualquer tecla para sair......");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ YDC/DownloadVideo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using YoutubeExplode;
+using YoutubeExplode.Videos;
+using YoutubeExplode.Videos.Streams;
+
+namespace YoutubeDownloaderAndConverter.YDC
+{
+    public class DownloadVideo
+    {
+        private string _url;
+        private YoutubeClient _client;
+
+        public DownloadVideo(string url)
+        {
+            _url = url;
+            _client = new YoutubeClient();
+        }
+
+        public async Task<bool> GetVideo(string savePath, string formato)
+        {
+            try
+            {
+                Video video = await _client.Videos.GetAsync(_url);
+                if (video == null)
+                    throw new Exception("Video inválido ou URL informada é inválida!");
+                await SaveVideo(video, savePath, formato);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+        }
+
+        private async Task SaveVideo(Video video, string path, string formato)
+        {
+            StreamManifest streamManifest = await _client.Videos.Streams.GetManifestAsync(_url);
+            IVideoStreamInfo videoFormat = null;
+            IStreamInfo musicFormat = null;
+
+            if (formato == "MP4")
+            {
+                videoFormat = streamManifest.GetVideoOnlyStreams()
+                    .Where(s => s.Container == Container.Mp4)
+                    .GetWithHighestVideoQuality();
+            }
+            else
+            {
+                musicFormat = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
+            }
+
+
+            Console.WriteLine("Fazendo download....");
+
+            await _client.Videos.Streams
+                .DownloadAsync(videoFormat != null ? videoFormat : musicFormat,
+                Path.Combine(path, $"{video.Title.Substring(0, 8)}." +
+                $"{(videoFormat != null ? videoFormat.Container : musicFormat.Container)}"));
+        }
+
+    }
+}
